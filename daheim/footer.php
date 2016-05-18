01de3ab2266aeefec1198f9d873fca5f00457e58
Gergo Ertli <egergo@gmail.com>
2016-05-18 09:30:15 +0300

    Newsletter signup form in footer

--- daheim/footer.php
+++ daheim/footer.php
@@ -10,9 +10,9 @@
 	</main><!-- main -->
 
 	<footer id="footer" role="contentinfo">
-					<div class="row" style="display: flex; margin: 0; margin: 8px 20px;">
+					<div class="row" style="display: flex; flex-wrap: wrap; margin: 0 20px 8px 20px;">
 						<div style="flex: 1 1 200px">
-							<h3 style="font-size: 16px; color: inherit; margin-top: 0">Impressum</h3>
+							<h3 style="font-size: 16px; color: inherit; margin-top: 8px">Impressum</h3>
 							<div>
 								Daheim gUG (haftungsbeschränkt)<br />
 								c/o Social Impact Lab Duisburg<br />
@@ -21,21 +21,36 @@
 							</div>
 						</div>
 						<div style="flex: 1 1 200px">
-							<h3 style="font-size: 16px; color: inherit; margin-top: 0">Kontakt</h3>
+							<h3 style="font-size: 16px; color: inherit; margin-top: 8px">Kontakt</h3>
 							<div>
-								Geschäfstführer: Madita Best<br />
+								<p>Geschäfstführer: Madita Best<br />
 								Tel: +49 201 6472 9928<br />
-								Email: <a href="mailto:hallo@daheimapp.de" target="_blank">hallo@daheimapp.de</a>
+								Email: <a href="mailto:hallo@daheimapp.de" target="_blank">hallo@daheimapp.de</a></p>
+							  <p><a href="/agb/">AGB</a> | <a href="/privacy/">Datenschutz</a></p>
 							</div>
 						</div>
 						<div style="flex: 1 1 200px">
-							<h3 style="font-size: 16px; color: inherit; margin-top: 0">Partner</h3>
-							<div><a href="http://duisburg.socialimpactlab.eu/">Social Impact Lab Duisburg<a/></div>
+							<h3 style="font-size: 16px; color: inherit; margin-top: 8px">Partner</h3>
+							<div><a href="http://duisburg.socialimpactlab.eu/">Social Impact Lab Duisburg</a></div>
 						</div>
 						<div style="flex: 1 1 200px">
-							<h3 style="font-size: 16px; color: inherit; margin-top: 0">Links</h3>
-							<div><a href="/agb/">AGB</a></div>
-							<div><a href="/privacy/">Datenschutz</a></div>
+							<h3 style="font-size: 16px; color: inherit; margin-top: 8px">Für Newsletter anmelden</h3>
+							<div>
+								<form action="//daheimapp.us12.list-manage.com/subscribe/post?u=c4d83a63f2e01438e01ffb425&amp;id=8090515741" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+									<p>
+										<label for="footer-mce-EMAIL">E-Mail-Adresse  <span class="asterisk">*</span></label>
+										<br/>
+										<input style="background: #555; color: #DDD; border-radius: 3px; border-color: #777; padding: 5px; width: 80%;" type="email" value="" name="EMAIL" id="footer-mce-EMAIL">
+									</p>
+
+									<p>
+										<label for="footer-mce-FNAME">Vorname</label>
+										<br/>
+										<input style="background: #555; color: #DDD; border-radius: 3px; border-color: #777; padding: 5px; width: 80%;" type="text" value="" name="FNAME" id="footer-mce-FNAME">
+									</p>
+									<p><input style="background: #555; color: #DDD; padding: 6px 12px; border-color: #777; border-radius: 3px;" type="submit" value="Anmelden" name="subscribe" class="button"></p>
+								</form>
+							</div>
 						</div>
 					</div>
 		<div id="footer-content" class="container">
