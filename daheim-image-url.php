119d0ef995746e576bc19d7b71aa8c9efb452708
Gergo Ertli <egergo@gmail.com>
2016-09-30 15:44:20 +0300

    Allow Azure HTTPS CNAME through daheim-image-url plugin

--- daheim-image-url.php
+++ daheim-image-url.php
@@ -2,10 +2,11 @@
 /*
 Plugin Name: Daheim Image URL
 Plugin URI: http://daheimapp.de
-Description: Normalize images uploaded to Azure
+Description: Disable srcsets and allow Azure Storage https CNAMEs
 Author: Gergő Ertli 
 Version: 1.0
 */
 
 // srcset urls always point to wp-content
 add_filter('wp_calculate_image_srcset_meta', '__return_null');
+add_filter('windows_azure_storage_allow_cname_https', '__return_true');
