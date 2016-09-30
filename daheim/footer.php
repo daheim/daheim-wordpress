b302ceaaf9f04d7690138d4611f181da5db6a76c
Gergo Ertli <egergo@gmail.com>
2016-09-30 15:45:54 +0300

    Fix typo in footer

--- daheim/footer.php
+++ daheim/footer.php
@@ -24,7 +24,7 @@
 						<div style="flex: 1 1 200px">
 							<h3 style="font-size: 16px; color: inherit; margin-top: 8px">Kontakt</h3>
 							<div>
-								<p>Geschäfstführung: Madita Best<br />
+								<p>Geschäftsführung: Madita Best<br />
 								Tel: +49 201 6472 9928<br />
 								Email: <a href="mailto:hallo@willkommen-daheim.org" target="_blank">hallo@willkommen-daheim.org</a></p>
 							  <p><a href="/agb/">AGB</a> | <a href="/privacy/">Datenschutz</a></p>
